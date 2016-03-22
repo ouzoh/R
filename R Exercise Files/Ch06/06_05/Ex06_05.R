48cf3133908bff5801c78e2bec1ec2c11a0e20cb
Ogaga Uzoh <friendsofglory@gmail.com>
2016-03-21 22:11:11 -0400

    Added files via upload
    "minor edits"

--- R Exercise Files/Ch06/06_05/Ex06_05.R
+++ R Exercise Files/Ch06/06_05/Ex06_05.R
@@ -7,15 +7,17 @@ google <- read.csv("google_correlate.csv", header = T)
 names(google)
 
 # One Way Anova
-anova1 <- aov(data_viz ~ region, data = google)
+anova1 <- aov(data_viz ~ region, data = google) #aov analysis of variance
 summary(anova1)
 
 # Two Way Factorial Design 
 # First way to specify
 anova2a <- aov(data_viz ~ 
-               region + stats_ed + region:stats_ed, 
-               data = google)
+               region + stats_ed + region:stats_ed, #: specify interaction btw 
+               data = google)                       # \n  region and stats_ed
 summary(anova2a)
+
+#< 0.05 p-value then it is statistically significant; 
 
 # Second way to specify
 anova2b <- aov(data_viz ~ 
